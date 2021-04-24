67e522a27fc0c28a104e57159bb6818fa1d66276
albarmo <moerhamsa@gmail.com>
2021-04-25 02:04:32 +0700

    feat: create navbar component responsive tablet and mobile phone

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.scss";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <Navbar menu={{ name: "propsname", path: "propsPath" }} />
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Navbar } from "../components";
 
 type Props = {
   name: string;
 };
 const Home: React.FC<Props> = ({ name }) => {
   return (
-    <>
+    <div className="container">
       <h1>Home</h1>
       <p>{name}</p>
-    </>
+    </div>
   );
 };
 
