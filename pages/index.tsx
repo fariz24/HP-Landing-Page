b870418f3033ab522a56fa904498d7e0a86418ef
Fariz Allawi <farizallawi24@gmail.com>
2021-11-09 13:44:08 +0700

    Setup project next.js and tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mt-12">
+      <Head>
+        <title>Harsha Pratala</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+
+        <h1 className="font-bold text-5xl">Hello Saya halaman utama</h1>
+    </div>
+  )
+}
+
+export default Home
